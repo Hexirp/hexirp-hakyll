fbed19b6f6aa4d3c67124b5b9575deeef29aa97b
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-17 17:37:26 +0900

    Update: Hakyll という仮のモジュールを作成する
    Hakyll を削除したが tests/ で参照されているためエラーになった。
    そのエラーを解決するためにこうする。

---
+++ hexirp-hakyll-core/tests/Hakyll.hs
@@ -0,0 +1,29 @@
+module Hakyll
+    ( module Hakyll.Core.Compiler
+    , module Hakyll.Core.Configuration
+    , module Hakyll.Core.File
+    , module Hakyll.Core.Identifier
+    , module Hakyll.Core.Identifier.Pattern
+    , module Hakyll.Core.Item
+    , module Hakyll.Core.Metadata
+    , module Hakyll.Core.Routes
+    , module Hakyll.Core.Rules
+    , module Hakyll.Core.UnixFilter
+    , module Hakyll.Core.Util.File
+    , module Hakyll.Core.Util.String
+    , module Hakyll.Core.Writable
+    ) where
+
+import           Hakyll.Core.Compiler
+import           Hakyll.Core.Configuration
+import           Hakyll.Core.File
+import           Hakyll.Core.Identifier
+import           Hakyll.Core.Identifier.Pattern
+import           Hakyll.Core.Item
+import           Hakyll.Core.Metadata
+import           Hakyll.Core.Routes
+import           Hakyll.Core.Rules
+import           Hakyll.Core.UnixFilter
+import           Hakyll.Core.Util.File
+import           Hakyll.Core.Util.String
+import           Hakyll.Core.Writable
