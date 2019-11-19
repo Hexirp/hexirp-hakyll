4eedfd690d882a002d96b496789b4f04d19ffdc0
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-19 23:02:12 +0900

    Update: 削除した pandoc 関係モジュールの使用を無くす

--- hexyll/tests/TestSuite.hs
+++ hexyll/tests/TestSuite.hs
@@ -14,9 +14,6 @@ import qualified Hexyll.Core.Runtime.Tests
 import qualified Hexyll.Web.CompressCss.Tests
 import qualified Hexyll.Web.Html.RelativizeUrls.Tests
 import qualified Hexyll.Web.Html.Tests
-#ifdef USE_PANDOC
-import qualified Hexyll.Web.Pandoc.FileType.Tests
-#endif
 import qualified Hexyll.Web.Template.Context.Tests
 import qualified Hexyll.Web.Template.Tests
 import qualified Hexyll.Web.Tags.Tests
@@ -29,9 +26,6 @@ main = defaultMain $ testGroup "Hexyll"
     , Hexyll.Web.CompressCss.Tests.tests
     , Hexyll.Web.Html.RelativizeUrls.Tests.tests
     , Hexyll.Web.Html.Tests.tests
-#ifdef USE_PANDOC
-    , Hexyll.Web.Pandoc.FileType.Tests.tests
-#endif
     , Hexyll.Web.Tags.Tests.tests
     , Hexyll.Web.Template.Context.Tests.tests
     , Hexyll.Web.Template.Tests.tests
