0333cf462459ce81278d14ba77298857d16ca21d
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-19 23:04:33 +0900

    Update: hexyll-pandoc のテストから削除したものを取り除く

--- hexyll-pandoc/tests/TestSuite.hs
+++ hexyll-pandoc/tests/TestSuite.hs
@@ -10,29 +10,16 @@ import           Test.Tasty                           (defaultMain, testGroup)
 
 
 --------------------------------------------------------------------------------
-import qualified Hexyll.Core.Runtime.Tests
-import qualified Hexyll.Web.CompressCss.Tests
-import qualified Hexyll.Web.Html.RelativizeUrls.Tests
-import qualified Hexyll.Web.Html.Tests
 #ifdef USE_PANDOC
 import qualified Hexyll.Web.Pandoc.FileType.Tests
 #endif
-import qualified Hexyll.Web.Template.Context.Tests
-import qualified Hexyll.Web.Template.Tests
-import qualified Hexyll.Web.Tags.Tests
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = defaultMain $ testGroup "Hexyll"
-    [ Hexyll.Core.Runtime.Tests.tests
-    , Hexyll.Web.CompressCss.Tests.tests
-    , Hexyll.Web.Html.RelativizeUrls.Tests.tests
-    , Hexyll.Web.Html.Tests.tests
+    [
 #ifdef USE_PANDOC
-    , Hexyll.Web.Pandoc.FileType.Tests.tests
+      Hexyll.Web.Pandoc.FileType.Tests.tests
 #endif
-    , Hexyll.Web.Tags.Tests.tests
-    , Hexyll.Web.Template.Context.Tests.tests
-    , Hexyll.Web.Template.Tests.tests
     ]
