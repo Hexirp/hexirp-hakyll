d3e1186bcac34d8ccaf718f54e407d33cd84aaab
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-23 17:57:29 +0900

    Revert "Update: Hexyll.Core.Provider.* の CPP を削除する"
    This reverts commit 76167bfc6107571613f9bbdd7e51cde83227bd55.

--- hexyll-core/src/Hexyll/Core/Provider/Internal.hs
+++ hexyll-core/src/Hexyll/Core/Provider/Internal.hs
@@ -36,9 +36,11 @@ import           System.FilePath        (addExtension, (</>))
 
 
 --------------------------------------------------------------------------------
+#if !MIN_VERSION_directory(1,2,0)
 import           Data.Time              (readTime)
 import           System.Locale          (defaultTimeLocale)
 import           System.Time            (formatCalendarTime, toCalendarTime)
+#endif
 
 
 --------------------------------------------------------------------------------
@@ -191,6 +193,10 @@ resourceModificationTime p i =
 --------------------------------------------------------------------------------
 fileModificationTime :: FilePath -> IO UTCTime
 fileModificationTime fp = do
+#if MIN_VERSION_directory(1,2,0)
+    getModificationTime fp
+#else
     ct <- toCalendarTime =<< getModificationTime fp
     let str = formatCalendarTime defaultTimeLocale "%s" ct
     return $ readTime defaultTimeLocale "%s" str
+#endif
