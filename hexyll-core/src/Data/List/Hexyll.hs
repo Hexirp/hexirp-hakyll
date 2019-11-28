57d7ee574368aca51ebad7892aad1b485ddd14ab
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-28 19:20:11 +0900

    Update: breakWhen の実装を効率が良いものへ
    base-4.12.0.0 の Data.List の break の GHC.List での実装を参考にした。

--- hexyll-core/src/Data/List/Hexyll.hs
+++ hexyll-core/src/Data/List/Hexyll.hs
@@ -7,9 +7,7 @@ import           Data.List
 
 -- | Like 'break', but can act on the entire tail of the list.
 breakWhen :: ([a] -> Bool) -> [a] -> ([a], [a])
-breakWhen predicate = go []
-  where
-    go buf []                = (reverse buf, [])
-    go buf (x : xs)
-        | predicate (x : xs) = (reverse buf, x : xs)
-        | otherwise          = go (x : buf) xs
+breakWhen _ xs@[]      = ([], xs)
+breakWhen p xs@(x:xs')
+  | p xs               = ([], xs)
+  | otherwise          = case breakWhen p xs' of { (ys, zs) -> (x:ys, zs); }
