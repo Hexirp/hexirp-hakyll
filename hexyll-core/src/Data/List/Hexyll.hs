32d309e04c886937bb15bf363c53541016f3eb1b
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-28 19:28:25 +0900

    Clean: Data.List.Hexyll を整形する

--- hexyll-core/src/Data/List/Hexyll.hs
+++ hexyll-core/src/Data/List/Hexyll.hs
@@ -1,13 +1,13 @@
 module Data.List.Hexyll
-    ( breakWhen
-    ) where
+  ( breakWhen
+  ) where
 
-import Prelude
-import Data.List
+  import Prelude
+  import Data.List
 
--- | Like 'break', but can act on the entire tail of the list.
-breakWhen :: ([a] -> Bool) -> [a] -> ([a], [a])
-breakWhen _ xs@[]      = ([], xs)
-breakWhen p xs@(x:xs')
-  | p xs               = ([], xs)
-  | otherwise          = case breakWhen p xs' of { (ys, zs) -> (x:ys, zs); }
+  -- | Like 'break', but can act on the entire tail of the list.
+  breakWhen :: ([a] -> Bool) -> [a] -> ([a], [a])
+  breakWhen _ xs@[]      = ([], xs)
+  breakWhen p xs@(x:xs')
+    | p xs               = ([], xs)
+    | otherwise          = case breakWhen p xs' of { (ys, zs) -> (x:ys, zs); }
