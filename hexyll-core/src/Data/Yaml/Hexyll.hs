b629ffb19e9fce9ab901929d581b13563322e795
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-29 16:21:05 +0900

    Update: toString をリファクタリングする
    ドキュメントを書いて、インデントを揃えて、 show を使わずに明示的に
    フォーマットを指定するようにした。

--- hexyll-core/src/Data/Yaml/Hexyll.hs
+++ hexyll-core/src/Data/Yaml/Hexyll.hs
@@ -8,15 +8,20 @@ import qualified Data.Vector as V
 import           Data.Yaml
 import           Data.Scientific
 
+-- | Convert 'Data.Yaml.Value' to 'String'. If 'toString' apply to a value
+-- which is not scalar (is 'Object', 'Array', or 'Null'), will return
+-- @Nothing@.
+--
+-- 'toString' make sure that numeric fields containing integer numbers are
+-- shown as integers (i.e., "42" instead of "42.0").
 toString :: Value -> Maybe String
-toString (String t)     = Just (T.unpack t)
-toString (Bool   True)  = Just "true"
-toString (Bool   False) = Just "false"
--- | Make sure that numeric fields containing integer numbers are shown as
--- | integers (i.e., "42" instead of "42.0").
-toString (Number d) | isInteger d = Just (formatScientific Fixed (Just 0) d)
-                    | otherwise   = Just (show d)
-toString _              = Nothing
+toString (String t)   = Just (T.unpack t)
+toString (Bool True)  = Just "true"
+toString (Bool False) = Just "false"
+toString (Number d)
+  | isInteger d       = Just (formatScientific Fixed (Just 0) d)
+  | otherwise         = Just (formatScientific Fixed Nothing d)
+toString _            = Nothing
 
 toList :: Value -> Maybe [Value]
 toList (Array a) = Just (V.toList a)
