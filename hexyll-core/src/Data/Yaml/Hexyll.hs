a5197271d89d0eee7113d5fea560dfc3c9ac3490
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-29 16:39:25 +0900

    Update: モジュールのドキュメントを追加する

--- hexyll-core/src/Data/Yaml/Hexyll.hs
+++ hexyll-core/src/Data/Yaml/Hexyll.hs
@@ -1,3 +1,12 @@
+-- |
+-- Module: Data.Yaml.Hexyll
+-- Copyright: (c) 2019 Hexirp
+-- License: Apache-2.0
+-- Maintainer: https://github.com/Hexirp/hexirp-hakyll
+-- Stability: stable
+-- Portability: portable
+--
+-- This module includes functions about "Data.Yaml".
 module Data.Yaml.Hexyll
   ( toString
   , toList
