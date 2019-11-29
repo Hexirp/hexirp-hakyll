7832c5d3abe2ee5098a7f2f68f47207bb9606fff
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-29 16:55:15 +0900

    Update: -Wtype-defaults を回避するために型注釈を付ける
    まあ、書き方は気に入らないけど現実の世界ってことで。

--- hexyll-core/test/Data/List/HexyllSpec.hs
+++ hexyll-core/test/Data/List/HexyllSpec.hs
@@ -8,19 +8,29 @@ module Data.List.HexyllSpec (spec) where
 
   spec :: Spec
   spec = do
+
     describe "breakWhen" $ do
+
       it "can see the tail" $ do
         breakWhen (\xs -> length xs == 3) [1,2,3,4,1,2,3,4]
-            `shouldBe` ([1,2,3,4,1], [2,3,4])
+            `shouldBe` ([1,2,3,4,1], [2,3,4] :: [Int])
+
       it "can be used like 'break'" $ do
         breakWhen (\xs -> head xs > 3) [1,2,3,4,1,2,3,4]
-            `shouldBe` ([1,2,3], [4,1,2,3,4])
+            `shouldBe` ([1,2,3], [4,1,2,3,4] :: [Int])
+
       it "applied to an empty list, then returns empty lists" $ do
-        breakWhen (\xs -> head xs > 3) [] `shouldBe` ([], [])
+        breakWhen (\xs -> head xs > 3) []
+            `shouldBe` ([], [] :: [Int])
+
       it "may return empty list on first element" $ do
-        breakWhen (const True) [1,2,3,4] `shouldBe` ([], [1,2,3,4])
+        breakWhen (const True) [1,2,3,4]
+            `shouldBe` ([], [1,2,3,4] :: [Int])
+
       it "may return empty list on second element" $ do
-        breakWhen (const False) [1,2,3,4] `shouldBe` ([1,2,3,4], [])
+        breakWhen (const False) [1,2,3,4]
+            `shouldBe` ([1,2,3,4], [] :: [Int])
+
       it "does not apply @p@ to an empty list" $ do
         breakWhen (\xs -> if null xs then undefined else False) [1,2,3,4]
-            `shouldBe` ([1,2,3,4], [])
+            `shouldBe` ([1,2,3,4], [] :: [Int])
