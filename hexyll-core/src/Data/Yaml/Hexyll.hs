64c43ded3b33eb315c28770cef4772a061d27228
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-29 16:31:01 +0900

    Update: Data.Yaml.Hexyll.toString のドキュメントを
    より詳細にする。また、メタデータも付けた。

--- hexyll-core/src/Data/Yaml/Hexyll.hs
+++ hexyll-core/src/Data/Yaml/Hexyll.hs
@@ -8,12 +8,14 @@ import qualified Data.Vector as V
 import           Data.Yaml
 import           Data.Scientific
 
--- | Convert 'Data.Yaml.Value' to 'String'. If 'toString' apply to a value
--- which is not scalar (is 'Object', 'Array', or 'Null'), will return
+-- | Convert 'Data.Yaml.Value' to 'String' for hexyll. If 'toString' apply to
+-- a value which is not a scalar (is 'Object', 'Array', or 'Null'), will return
 -- @Nothing@.
 --
 -- 'toString' make sure that numeric fields containing integer numbers are
 -- shown as integers (i.e., "42" instead of "42.0").
+--
+-- @since 0.1.0.0
 toString :: Value -> Maybe String
 toString (String t)   = Just (T.unpack t)
 toString (Bool True)  = Just "true"
