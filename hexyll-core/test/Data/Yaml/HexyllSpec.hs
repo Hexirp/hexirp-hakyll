472b512aa6407dfa76e77548fbca24a8bebb43c5
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-30 12:57:58 +0900

    Update: Data.Yaml.Hexyll の単体テストを追加する

---
+++ hexyll-core/test/Data/Yaml/HexyllSpec.hs
@@ -0,0 +1,48 @@
+module Data.Yaml.HexyllSpec (spec) where
+
+  import Prelude
+  import Test.Hspec
+
+  import qualified Data.Text    as T
+  import qualified Data.Vector  as V
+  import qualified Data.HashMap as H
+  import           Data.Yaml
+  import           Data.Scientific
+
+  import Data.Yaml.Hexyll
+
+  spec :: Spec
+  spec = do
+
+    describe "toString" $ do
+
+      it "works at String" $ do
+        toString (T.pack "foo") `shouldBe` Just "foo"
+
+      it "works at Bool" $ do
+        toString (Bool True) `shouldBe` Just "true"
+        toString (Bool False) `shouldBe` Just "false"
+
+      it "works at Number (integer)" $ do
+        toString (Number (scientific 12 1)) `shouldBe` Just "120"
+
+      it "works at Number (fraction)" $ do
+        toString (Number (scientific 12 (-1)) `shouldBe` Just "1.2"
+
+      it "works at Number (too big integer)" $ do
+        toString (Number (scientific 12 7) `shouldBe` Just "120000000"
+
+      it "does not work at Array" $ do
+        toString (Array V.empty) `shouldBe` Nothing
+
+      it "does not work at Object" $ do
+        toString (Object H.empty) `shouldBe` Nothing
+
+      it "does not work at Null" $ do
+        toString Null `shouldBe` Nothing
+
+    describe "toList" $
+
+      it "works at Array" $ do
+        toString (Array (V.fromList [Bool True, Bool False]))
+            `shouldBe` Just [Bool True, Bool False]
