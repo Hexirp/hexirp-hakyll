c133f0f8777d17077d149b7394ebf2705fc6b9a3
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-12-06 20:41:24 +0900

    Update: defaultConfiguration のテストを削除する
    doctest でやりつくされているので不要なので削除する。

--- hexyll-core/test/Hexyll/Core/ConfigurationSpec.hs
+++ hexyll-core/test/Hexyll/Core/ConfigurationSpec.hs
@@ -6,10 +6,4 @@ module Hexyll.Core.ConfigurationSpec (spec) where
   import Hexyll.Core.Configuration
 
   spec :: Spec
-  spec = do
-
-    describe "defaultConfiguration" $ do
-
-      it "has a destination directory '_site' in the field" $ do
-
-        destinationDirectory defaultConfiguration `shouldBe` "_site"
+  spec = return ()
