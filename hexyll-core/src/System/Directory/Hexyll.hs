e741921a16598f9271314b361d334a820c72ebc3
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-12-06 20:14:40 +0900

    Add: System.Directory.Hexyll のドキュメントを書く

--- hexyll-core/src/System/Directory/Hexyll.hs
+++ hexyll-core/src/System/Directory/Hexyll.hs
@@ -1,3 +1,12 @@
+-- |
+-- Module:      System.Directory.Hexyll
+-- Copyright:   (c) 2019 Hexirp
+-- License:     Apache-2.0
+-- Maintainer:  https://github.com/Hexirp/hexirp-hakyll
+-- Stability:   unstable
+-- Portability: portable
+--
+-- This module includes additional functions of "System.Directory".
 module System.Directory.Hexyll
   ( inDir
   ) where
@@ -9,6 +18,19 @@ module System.Directory.Hexyll
   import System.FilePath
   import System.Directory
 
+  -- | @inDir path dir@ checks that @path@ is under @dir@. For example, @inDir
+  -- "foo/bar/a.txt" "foo/"@ may be equal to @return True@.
+  --
+  -- >>> inDir "foo/a.txt" "foo/"
+  -- True
+  --
+  -- >>> inDir "foo/bar/a.txt" "foo/"
+  -- True
+  --
+  -- >>> inDir "foo/baz/a.txt" "foo/baa/"
+  -- False
+  --
+  -- @since 0.1.0.0
   inDir :: FilePath -> FilePath -> IO Bool
   inDir path dir = if isAbsolute path
     then do
