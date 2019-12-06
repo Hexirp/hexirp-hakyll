b130e29155bd8f971cfb5aa8784556300615170b
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-12-06 20:50:22 +0900

    Add: defaultIgnoreFile のテストを

--- hexyll-core/test/Hexyll/Core/ConfigurationSpec.hs
+++ hexyll-core/test/Hexyll/Core/ConfigurationSpec.hs
@@ -6,4 +6,31 @@ module Hexyll.Core.ConfigurationSpec (spec) where
   import Hexyll.Core.Configuration
 
   spec :: Spec
-  spec = return ()
+  spec = do
+
+    describe "defaultIgnoreFile" $ do
+
+      it "ignore files is prefix of '.'" $ do
+        defaultIgnoreFile "." `shouldBe` True
+        defaultIgnoreFile ".." `shouldBe` True
+        defaultIgnoreFile ".gitignore" `shouldBe` True
+        defaultIgnoreFile ".stack-work/" `shouldBe` True
+
+      it "ignore files is prefix of '#'" $ do
+        defaultIgnoreFile "#Main.hs#" `shouldBe` True
+        defaultIgnoreFile "#s" `shouldBe` True
+        defaultIgnoreFile "#" `shouldBe` True
+
+      it "ignore files is suffix of '~'" $ do
+        defaultIgnoreFile "foo~" `shouldBe` True
+        defaultIgnoreFile "~/" `shouldBe` True
+        defaultIgnoreFile "herobrine~/" `shouldBe` True
+
+      it "ignore files is suffix of '.swp'" $ do
+        defaultIgnoreFile "a.txt.swp" `shouldBe` True
+
+      it "do not ignore other files" $ do
+        defaultIgnoreFile "ma." `shouldBe` False
+        defaultIgnoreFile "hash#" `shouldBe` False
+        defaultIgnoreFile "~foo" `shouldBe` False
+        defaultIgnoreFile "a.yxy.swp.log" `shouldBe` False
