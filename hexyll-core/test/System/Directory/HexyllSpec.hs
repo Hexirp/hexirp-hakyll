9a470f571e97b0029eb4dc284e28113cb7585c05
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-12-13 20:55:18 +0900

    Update: パスのクォートにシングルクォーテーションを
    ドキュメントレベルの変更。

--- hexyll-core/test/System/Directory/HexyllSpec.hs
+++ hexyll-core/test/System/Directory/HexyllSpec.hs
@@ -13,19 +13,19 @@ module System.Directory.HexyllSpec (spec) where
 
     describe "inDir" $ do
 
-      it "works in normally files \"foo/a.txt\" and \"foo/\"" $ do
+      it "works in normally files 'foo/a.txt' and 'foo/'" $ do
         inDir "foo/a.txt" "foo/" `shouldReturn` True
 
-      it "works in normally files \"foo/bar/a.txt\" and \"foo/\"" $ do
+      it "works in normally files 'foo/bar/a.txt' and 'foo/'" $ do
         inDir "foo/bar/a.txt" "foo/" `shouldReturn` True
 
-      it "works in normally files \"foo/baz/a.txt\" and \"foo/bar/\"" $ do
+      it "works in normally files 'foo/baz/a.txt' and 'foo/bar/'" $ do
         inDir "foo/baz/a.txt" "foo/bar/" `shouldReturn` False
 
-      it "works in normally files \"foo/baz/a.txt\" and \"foo\"" $ do
+      it "works in normally files 'foo/baz/a.txt' and 'foo" $ do
         inDir "foo/baz/a.txt" "foo" `shouldReturn` True
 
-      it "works in normally files \"foo/bar\" and \"foo\"" $ do
+      it "works in normally files 'foo/bar' and 'foo'" $ do
         inDir "foo/bar" "foo" `shouldReturn` True
 
       it "works with the special directory @.@" $ do
