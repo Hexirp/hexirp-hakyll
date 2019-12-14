0796de62d681bdb76356194bf8027330d6959d97
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-12-14 10:07:31 +0900

    Update: 一つの it に一つの expectation とする
    どこで失敗したかメッセージから分かりやすくするため。

--- hexyll-core/test/Control/Monad/HexyllSpec.hs
+++ hexyll-core/test/Control/Monad/HexyllSpec.hs
@@ -12,13 +12,18 @@ module Control.Monad.HexyllSpec (spec) where
 
     describe "orM" $ do
 
-      it "is a version of 'or' lifted to a monad" $ do
+      it "is a version of 'or' lifted to a monad ([FFF]=F)" $ do
         orM [Just False, Just False, Just False] `shouldBe` Just False
-        orM [Just True, Just False] `shouldBe` Just True
+
+      it "is a version of 'or' lifted to a monad ([FFT]=T)" $ do
         orM [Just False, Just False, Just True] `shouldBe` Just True
+
+      it "is a version of 'or' lifted to a monad ([]=F)" $ do
         orM [] `shouldBe` Just False
 
-      it "retains the short-circuiting behaveiour" $ do
+      it "retains the short-circuiting behaveiour ([FTU]=T)" $ do
         orM [Just False, Just True, error "evaluated"] `shouldBe` Just True
+
+      it "retains the short-circuiting behaveiour ([FFU]=U)" $ do
         evaluate (orM [Just False, Just False, error "evaluated"])
             `shouldThrow` errorCall "evaluated"
--- hexyll-core/test/Hexyll/Core/ConfigurationSpec.hs
+++ hexyll-core/test/Hexyll/Core/ConfigurationSpec.hs
@@ -10,31 +10,53 @@ module Hexyll.Core.ConfigurationSpec (spec) where
 
     describe "defaultIgnoreFile" $ do
 
-      it "ignore files is prefix of '.'" $ do
+      it "ignore files is prefix of '.' ('.')" $ do
         defaultIgnoreFile "." `shouldBe` True
+
+      it "ignore files is prefix of '.' ('..')" $ do
         defaultIgnoreFile ".." `shouldBe` True
+
+      it "ignore files is prefix of '.' ('.gitignore')" $ do
         defaultIgnoreFile ".gitignore" `shouldBe` True
 
-      it "ignore files is prefix of '#'" $ do
+      it "ignore files is prefix of '#' ('#Main.hs#')" $ do
         defaultIgnoreFile "#Main.hs#" `shouldBe` True
+
+      it "ignore files is prefix of '#' ('#s')" $ do
         defaultIgnoreFile "#s" `shouldBe` True
+
+      it "ignore files is prefix of '#' ('#')" $ do
         defaultIgnoreFile "#" `shouldBe` True
 
-      it "ignore files is suffix of '~'" $ do
+      it "ignore files is suffix of '~' ('~foo')" $ do
         defaultIgnoreFile "foo~" `shouldBe` True
+
+      it "ignore files is suffix of '~' ('pya/~')" $ do
         defaultIgnoreFile "pya/~" `shouldBe` True
+
+      it "ignore files is suffix of '~' ('mu/herobrine~')" $ do
         defaultIgnoreFile "mu/herobrine~" `shouldBe` True
 
       it "ignore files is suffix of '.swp'" $ do
         defaultIgnoreFile "a.txt.swp" `shouldBe` True
 
-      it "do not ignore other files" $ do
+      it "do not ignore other files ('ma.')" $ do
         defaultIgnoreFile "ma." `shouldBe` False
+
+      it "do not ignore other files ('hash#')" $ do
         defaultIgnoreFile "hash#" `shouldBe` False
+
+      it "do not ignore other files ('~foo')" $ do
         defaultIgnoreFile "~foo" `shouldBe` False
+
+      it "do not ignore other files ('a.yxy.swp.log')" $ do
         defaultIgnoreFile "a.yxy.swp.log" `shouldBe` False
 
-      it "applied to a directory, returns False" $ do
+      it "applied to a directory, returns False ('eee/')" $ do
         defaultIgnoreFile "eee/" `shouldBe` False
+
+      it "applied to a directory, returns False ('too/eee/')" $ do
         defaultIgnoreFile "too/eee/" `shouldBe` False
+
+      it "applied to a directory, returns False ('foo~/')" $ do
         defaultIgnoreFile "foo~/" `shouldBe` False
