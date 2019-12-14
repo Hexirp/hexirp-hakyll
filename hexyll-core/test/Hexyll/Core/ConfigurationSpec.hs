0ebdbecbd0eb20a8f2b8ffde6621f0c33c1be9e0
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-12-14 15:07:40 +0900

    Update: Path を使うようにテストを修正する

--- hexyll-core/test/Hexyll/Core/ConfigurationSpec.hs
+++ hexyll-core/test/Hexyll/Core/ConfigurationSpec.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Hexyll.Core.ConfigurationSpec (spec) where
 
   import Prelude
   import Test.Hspec
+
+  import Path
 
   import Hexyll.Core.Configuration
 
@@ -10,53 +14,38 @@ module Hexyll.Core.ConfigurationSpec (spec) where
 
     describe "defaultIgnoreFile" $ do
 
-      it "ignore files is prefix of '.' ('.')" $ do
-        defaultIgnoreFile "." `shouldBe` True
-
-      it "ignore files is prefix of '.' ('..')" $ do
-        defaultIgnoreFile ".." `shouldBe` True
-
       it "ignore files is prefix of '.' ('.gitignore')" $ do
-        defaultIgnoreFile ".gitignore" `shouldBe` True
+        defaultIgnoreFile $(mkRelFile ".gitignore") `shouldBe` True
 
       it "ignore files is prefix of '#' ('#Main.hs#')" $ do
-        defaultIgnoreFile "#Main.hs#" `shouldBe` True
+        defaultIgnoreFile $(mkRelFile "#Main.hs#") `shouldBe` True
 
       it "ignore files is prefix of '#' ('#s')" $ do
-        defaultIgnoreFile "#s" `shouldBe` True
+        defaultIgnoreFile $(mkRelFile "#s") `shouldBe` True
 
       it "ignore files is prefix of '#' ('#')" $ do
-        defaultIgnoreFile "#" `shouldBe` True
+        defaultIgnoreFile $(mkRelFile "#") `shouldBe` True
 
       it "ignore files is suffix of '~' ('~foo')" $ do
-        defaultIgnoreFile "foo~" `shouldBe` True
+        defaultIgnoreFile $(mkRelFile "foo~") `shouldBe` True
 
       it "ignore files is suffix of '~' ('pya/~')" $ do
-        defaultIgnoreFile "pya/~" `shouldBe` True
+        defaultIgnoreFile $(mkRelFile "pya/~") `shouldBe` True
 
       it "ignore files is suffix of '~' ('mu/herobrine~')" $ do
-        defaultIgnoreFile "mu/herobrine~" `shouldBe` True
+        defaultIgnoreFile $(mkRelFile "mu/herobrine~") `shouldBe` True
 
       it "ignore files is suffix of '.swp'" $ do
-        defaultIgnoreFile "a.txt.swp" `shouldBe` True
+        defaultIgnoreFile $(mkRelFile "a.txt.swp") `shouldBe` True
 
       it "do not ignore other files ('ma.')" $ do
-        defaultIgnoreFile "ma." `shouldBe` False
+        defaultIgnoreFile $(mkRelFile "ma.") `shouldBe` False
 
       it "do not ignore other files ('hash#')" $ do
-        defaultIgnoreFile "hash#" `shouldBe` False
+        defaultIgnoreFile $(mkRelFile "hash#") `shouldBe` False
 
       it "do not ignore other files ('~foo')" $ do
-        defaultIgnoreFile "~foo" `shouldBe` False
+        defaultIgnoreFile $(mkRelFile "~foo") `shouldBe` False
 
       it "do not ignore other files ('a.yxy.swp.log')" $ do
-        defaultIgnoreFile "a.yxy.swp.log" `shouldBe` False
-
-      it "applied to a directory, returns False ('eee/')" $ do
-        defaultIgnoreFile "eee/" `shouldBe` False
-
-      it "applied to a directory, returns False ('too/eee/')" $ do
-        defaultIgnoreFile "too/eee/" `shouldBe` False
-
-      it "applied to a directory, returns False ('foo~/')" $ do
-        defaultIgnoreFile "foo~/" `shouldBe` False
+        defaultIgnoreFile $(mkRelFile "a.yxy.swp.log") `shouldBe` False
