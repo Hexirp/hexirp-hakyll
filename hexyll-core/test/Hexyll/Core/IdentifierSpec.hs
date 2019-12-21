c11426d31e9e5318c246ebb4e030085bb1445934
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-12-21 19:24:21 +0900

    Update: テストの中身を書く
    get と set についても書かないと。

--- hexyll-core/test/Hexyll/Core/IdentifierSpec.hs
+++ hexyll-core/test/Hexyll/Core/IdentifierSpec.hs
@@ -1,7 +1,34 @@
+{-# LANGUAGE CPP #-}
+
 module Hexyll.Core.IdentifierSpec (spec) where
 
   import Prelude
   import Test.Hspec
 
   spec :: Spec
-  spec = return ()
+  spec = do
+
+    describe "fromFilePath" $ do
+
+      it "can parse 'foo.txt'" $ do
+        rnf (fromFilePath "foo.txt") `shouldBe` ()
+
+      it "can not parse 'foo/'" $ do
+        (evaluate $ fromFilePath "foo/") `shouldThrow` anyErrorCall
+
+      it "can not parse '/foo.md'" $ do
+        (evaluate $ fromFilePath "/foo.md") `shouldThrow` anyErrorCall
+
+    describe "toFilePath" $ do
+#if defined(mingw32_HOST_OS) || defined(__MINGW32__)
+
+      it "returns a path with '/' separated" $ do
+        (toFilePath $ fromFilePath "foo\\bar.md") `shouldBe` "foo\\bar.md"
+
+      it "returns a path with '/' separated (form posix)" $ do
+        (toFilePath $ fromFilePath "foo/bar.md") `shouldBe` "foo\\bar.md"
+#else
+
+      it "returns a path with '/' separated" $ do
+        (toFilePath $ fromFilePath "foo/bar.md") `shouldBe` "foo/bar.md"
+#endif
