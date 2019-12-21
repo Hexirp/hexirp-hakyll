0558904efb4b053e028f9e9c4238436676694348
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-12-21 18:05:02 +0900

    Update: モジュールのドキュメントを

--- hexyll-core/src/Hexyll/Core/Identifier.hs
+++ hexyll-core/src/Hexyll/Core/Identifier.hs
@@ -1,14 +1,24 @@
 {-# LANGUAGE DeriveDataTypeable         #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
--- | An identifier is a type used to uniquely identify an item. An identifier is
--- conceptually similar to a file path. Examples of identifiers are:
+-- |
+-- Module:      Hexyll.Core.Identifier
+-- Copyright:   (c) 2019 Hexirp
+-- License:     Apache-2.0
+-- Maintainer:  https://github.com/Hexirp/hexirp-hakyll
+-- Stability:   stable
+-- Portability: portable
+--
+-- This module defines 'Identifier', a type used to uniquely identify an item.
+-- An identifier is conceptually similar to a file path. Examples of
+-- identifiers are:
 --
 -- * @posts/foo.markdown@
---
 -- * @index@
---
 -- * @error/404@
+--
+-- A 'Identifier' value can have its version. The information about version is
+-- used inside the library.
 module Hexyll.Core.Identifier
   ( Identifier
   , fromFilePath
