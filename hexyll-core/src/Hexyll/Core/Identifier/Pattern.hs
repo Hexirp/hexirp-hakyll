5b0b0c5d0c8ea88feb17fd779d39d886a4e29f28
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-12-30 23:34:39 +0900

    Clean: フォーマットをする

--- hexyll-core/src/Hexyll/Core/Identifier/Pattern.hs
+++ hexyll-core/src/Hexyll/Core/Identifier/Pattern.hs
@@ -1,49 +1,49 @@
 module Hexyll.Core.Identifier.Pattern where
 
-import Prelude
+  import Prelude
 
-import System.FilePath.Glob as Glob
+  import System.FilePath.Glob as Glob
 
-import Text.Regex.TDFA ((=~))
+  import Text.Regex.TDFA ((=~))
 
-import Hexyll.Core.Identifier
+  import Hexyll.Core.Identifier
 
-data PrimPattern
-  = Glob Glob.Pattern
-  | Regex String
-  | Version (Maybe String)
-  deriving (Eq, Show)
+  data PrimPattern
+    = Glob Glob.Pattern
+    | Regex String
+    | Version (Maybe String)
+    deriving (Eq, Show)
 
-newtype Pattern = Pattern { runPattern :: Identifier -> Bool }
+  newtype Pattern = Pattern { runPattern :: Identifier -> Bool }
 
-compile :: PrimPattern -> Pattern
-compile (Glob p) = \i -> Glob.match p (toFilePath i)
-compile (Regex r) = \i -> toFilePath i =~ r
-compile (Version v) = \i -> getIdentVersion i == v
+  compile :: PrimPattern -> Pattern
+  compile (Glob p) = \i -> Glob.match p (toFilePath i)
+  compile (Regex r) = \i -> toFilePath i =~ r
+  compile (Version v) = \i -> getIdentVersion i == v
 
-fromGlob :: String -> Pattern
-fromGlob = compile . Glob . Glob.compile
+  fromGlob :: String -> Pattern
+  fromGlob = compile . Glob . Glob.compile
 
-fromList :: [Identifier] -> Pattern
-fromList = foldr (.||.) nothing
+  fromList :: [Identifier] -> Pattern
+  fromList = foldr (.||.) nothing
 
-fromRegex :: String -> Pattern
-fromRegex = compile . Regex
+  fromRegex :: String -> Pattern
+  fromRegex = compile . Regex
 
-fromVersion :: Maybe String -> Pattern
-fromVersion = compile . Version
+  fromVersion :: Maybe String -> Pattern
+  fromVersion = compile . Version
 
-hasVersion :: String -> Pattern
-hasVersion = fromVersion . Just
+  hasVersion :: String -> Pattern
+  hasVersion = fromVersion . Just
 
-hasNoVersion :: Pattern
-hasNoVersion = fromVersion Nothing
+  hasNoVersion :: Pattern
+  hasNoVersion = fromVersion Nothing
 
-(.&&.) :: Pattern -> Pattern -> Pattern
-Pattern f .&&. Pattern g = Pattern $ \i -> f i && g i
+  (.&&.) :: Pattern -> Pattern -> Pattern
+  Pattern f .&&. Pattern g = Pattern $ \i -> f i && g i
 
-(.||.) :: Pattern -> Pattern -> Pattern
-Pattern f .||. Pattern g = Pattern $ \i -> f i || g i
+  (.||.) :: Pattern -> Pattern -> Pattern
+  Pattern f .||. Pattern g = Pattern $ \i -> f i || g i
 
-complement :: Pattern -> Pattern
-complement (Pattern f) = Pattern $ \i -> not $ f i
+  complement :: Pattern -> Pattern
+  complement (Pattern f) = Pattern $ \i -> not $ f i
