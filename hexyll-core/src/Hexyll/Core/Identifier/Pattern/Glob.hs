3adea4a3a6ad52b7fa6655465a69e3eb746f455b
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2020-01-04 18:58:31 +0900

    Add: Identifier.Pattern.Glob のドキュメントを

--- hexyll-core/src/Hexyll/Core/Identifier/Pattern/Glob.hs
+++ hexyll-core/src/Hexyll/Core/Identifier/Pattern/Glob.hs
@@ -17,20 +17,31 @@ module Hexyll.Core.Identifier.Pattern.Glob where
 
   import qualified System.FilePath.Glob as Glob
 
+  -- | The wrapper of 'Glob.Pattern'.
+  --
+  -- @since 0.1.0.0
   data Pattern = Pattern { unPattern :: Glob.Pattern }
     deriving (Eq, Show)
 
+  -- | @since 0.1.0.0
   instance IsString Pattern where
     fromString = Pattern . fromString
 
+  -- | @since 0.1.0.0
   instance Binary Pattern where
     put = put . Glob.decompile . unPattern
     get = Pattern . Glob.compile <$> get
 
+  -- | @since 0.1.0.0
   instance NFData Pattern where
     rnf (Pattern x) = rnf (decompile x) `seq` ()
     -- This may be incomplete. But it is necessary there are no instances
     -- @'NFData' 'Glob.Pattern'@.
 
+  -- | Match a 'String' with a 'Pattern'.
+  --
+  -- The function is a wrapper function of 'Glob.match'.
+  --
+  -- @since 0.1.0.0
   match :: String -> Pattern -> Bool
   match s (Pattern p) = Glob.match p s
