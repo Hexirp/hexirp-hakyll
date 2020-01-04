000316a2c7edf3708c9db625faae91453046bf89
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2020-01-04 15:23:27 +0900

    Update: モジュールのドキュメントをより詳しく

--- hexyll-core/src/Hexyll/Core/Identifier/Pattern.hs
+++ hexyll-core/src/Hexyll/Core/Identifier/Pattern.hs
@@ -8,12 +8,14 @@
 --
 -- This module defines a type of pattern matching to 'Identifier'.
 --
--- 'PrimPattern' is the primitive patterns. 'PatternExpr' is the expressions of
--- patterns. 'PatternConj' is a conjunction of 'PatternExpr's. 'PatternDisj' is
--- a disjunction of 'PatternExpr's. 'Pattern' is the patterns.
+-- 'PrimPattern' is a primitive pattern. 'PatternExpr' is an expression of
+-- pattern. 'PatternConj' is a conjunction of 'PatternExpr's. 'PatternDisj' is
+-- a disjunction of 'PatternExpr's. 'Pattern' is a pattern.
 --
 -- I recommend to use 'PatternExpr'. If you want to the instance of 'Monoid',
--- you can use 'PatternConj' or 'PatternDisj'.
+-- you can use 'PatternConj' or 'PatternDisj'. I was able to define the
+-- instance @'Monoid' 'PatternExpr'@ with @(.&&.)@ or @(.||.)@. But, it is not
+-- a real monoid because of @x .&&. (y .&&. z) \= (x .&&. y) .&&. z@.
 module Hexyll.Core.Identifier.Pattern where
 
   import Prelude
