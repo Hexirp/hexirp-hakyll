1ebd40a5ddd895518fd39e81940f455572fdb104
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2020-01-04 18:20:04 +0900

    Add: Glob.Pattern のラッパーを作成する
    主に NFData を実装するためのものだが、他にも PrimPattern の
    インスタンスの定義が簡単になるというのも期待している。

---
+++ hexyll-core/src/Hexyll/Core/Identifier/Pattern/Glob.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+-- |
+-- Module:      Hexyll.Core.Identifier.Pattern
+-- Copyright:   (c) 2019 Hexirp
+-- License:     Apache-2.0
+-- Maintainer:  https://github.com/Hexirp/hexirp-hakyll
+-- Stability:   unstable
+-- Portability: non-portable (compiler: GHC, extensions: DeriveGeneric)
+--
+-- This module defines a type for glob patterns.
+module Hexyll.Core.Identifier.Pattern.Glob where
+
+  import Prelude
+  import GHC.Generic
+
+  import Control.NFData (NFData (..)
+  import Data.String    (IsString (..))
+  import Data.Binary    (Binary (..))
+
+  import System.FilePath.Glob as Glob
+
+  data Pattern = Pattern { unPattern :: Glob.Pattern }
+    deriving (Eq, Show, Generic)
+
+  instance IsString Pattern where
+    fromString = Pattern . fromString
+
+  instance Binary Pattern where
+    put = put . Glob.decompile . unPattern
+    get = Pattern . Glob.compile <$> get
+
+  instance NFData Pattern
