6ba44488cd6160b8003b45d1b5091bcf0cc5b5d4
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2020-01-11 12:43:33 +0900

    Update: 理想の設計を示す

--- hexyll-core/src/Hexyll/Core/Dependencies.hs
+++ hexyll-core/src/Hexyll/Core/Dependencies.hs
@@ -29,32 +29,38 @@ import           Hexyll.Core.Identifier.Pattern
 
 
 --------------------------------------------------------------------------------
-data Dependency
-    = PatternDependency PatternExpr (Set Identifier)
-    | IdentifierDependency Identifier
-    deriving (Show, Typeable)
+newtype Dependency = Dependency { unDependency :: PatternExpr }
+  deriving (Eq, Show)
 
-
---------------------------------------------------------------------------------
 instance Binary Dependency where
-    put (PatternDependency p is) = putWord8 0 >> put p >> put is
-    put (IdentifierDependency i) = putWord8 1 >> put i
-    get = getWord8 >>= \t -> case t of
-        0 -> PatternDependency <$> get <*> get
-        1 -> IdentifierDependency <$> get
-        _ -> error "Data.Binary.get: Invalid Dependency"
+  put (Dependency x) = put x
+  get = Dependency <$> get
 
+newtype DependencyFacts
+  = DependencyFacts { unDependencyFacts :: Map Identifier [Dependency] }
+  deriving (Eq, Show)
 
---------------------------------------------------------------------------------
-type DependencyFacts = Map Identifier [Dependency]
+newtype DependencyCache
+  = DependencyCache { unDependencyCache :: Map Identifier [Identifier] }
+  deriving (Eq, Show)
 
+newtype DependencyLog = DependencyLog { unDependencyLog :: String -> String }
 
---------------------------------------------------------------------------------
+instance Semigroup DependencyLog where
+  DependencyLog x <> DependencyLog y = DependencyLog (x <> y)
+
+instance Monoid DependencyLog where
+  mempty = DependencyLog mempty
+
+type KnownIdentifierList = [Identifier]
+type MarkedIdentifierList = [Identifier]
+
 outOfDate
-    :: [Identifier]     -- ^ All known identifiers
-    -> Set Identifier   -- ^ Initially out-of-date resources
-    -> DependencyFacts  -- ^ Old dependency facts
-    -> (Set Identifier, DependencyFacts, [String])
+  :: KnownIdentifierList
+  -> MarkedIdentifierList
+  -> DependencyFacts
+  -> DependencyCache
+  -> (MarkedIdentifierList, DependencyCache, DependencyLog)
 outOfDate universe ood oldFacts =
     let (_, state, logs) = runRWS rws universe (DependencyState oldFacts ood)
     in (dependencyOod state, dependencyFacts state, logs)
