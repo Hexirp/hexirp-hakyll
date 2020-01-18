fb4835859a1d8c525348431193e67037b586af6b
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2020-01-18 20:17:15 +0900

    Fix: Hexyll.Core.IdentifierSpec
      fromFilePath -> ufromFilePath

--- hexyll-core/test/Hexyll/Core/IdentifierSpec.hs
+++ hexyll-core/test/Hexyll/Core/IdentifierSpec.hs
@@ -18,41 +18,41 @@ module Hexyll.Core.IdentifierSpec (spec) where
     describe "show @Identifier" $ do
 
       it "normally works" $ do
-        show (fromFilePath "a.txt") `shouldBe` "a.txt"
+        show (ufromFilePath "a.txt") `shouldBe` "a.txt"
 
       it "normally works with the version" $ do
-        show (setIdentVersion (Just "pdf") $ fromFilePath "a.txt")
+        show (setIdentVersion (Just "pdf") $ ufromFilePath "a.txt")
             `shouldBe` "a.txt (pdf)"
 
-    describe "fromFilePath" $ do
+    describe "ufromFilePath" $ do
 
       it "can parse 'foo.txt'" $ do
-        rnf (fromFilePath "foo.txt") `shouldBe` ()
+        rnf (ufromFilePath "foo.txt") `shouldBe` ()
 
       it "can not parse 'foo/'" $ do
-        (evaluate $ fromFilePath "foo/") `shouldThrow` anyErrorCall
+        (evaluate $ ufromFilePath "foo/") `shouldThrow` anyErrorCall
 #if defined(mingw32_HOST_OS) || defined(__MINGW32__)
 
       it "can not parse 'C:\\foo.md'" $ do
-        (evaluate $ fromFilePath "C:\\foo.md") `shouldThrow` anyErrorCall
+        (evaluate $ ufromFilePath "C:\\foo.md") `shouldThrow` anyErrorCall
 #else
 
       it "can not parse '/foo.md'" $ do
-        (evaluate $ fromFilePath "/foo.md") `shouldThrow` anyErrorCall
+        (evaluate $ ufromFilePath "/foo.md") `shouldThrow` anyErrorCall
 #endif
 
     describe "toFilePath" $ do
 #if defined(mingw32_HOST_OS) || defined(__MINGW32__)
 
       it "returns a path with '\\' separated" $ do
-        (toFilePath $ fromFilePath "foo\\bar.md") `shouldBe` "foo\\bar.md"
+        (toFilePath $ ufromFilePath "foo\\bar.md") `shouldBe` "foo\\bar.md"
 
       it "returns a path with '\\' separated (posix style)" $ do
-        (toFilePath $ fromFilePath "foo/bar.md") `shouldBe` "foo\\bar.md"
+        (toFilePath $ ufromFilePath "foo/bar.md") `shouldBe` "foo\\bar.md"
 #else
 
       it "returns a path with '/' separated" $ do
-        (toFilePath $ fromFilePath "foo/bar.md") `shouldBe` "foo/bar.md"
+        (toFilePath $ ufromFilePath "foo/bar.md") `shouldBe` "foo/bar.md"
 #endif
 
     describe "getIdentVersion" $ do
@@ -73,7 +73,7 @@ module Hexyll.Core.IdentifierSpec (spec) where
 
   -- The mock without a identifier version
   mock_Identifier_0 :: Identifier
-  mock_Identifier_0 = fromFilePath "foo.c"
+  mock_Identifier_0 = ufromFilePath "foo.c"
 
   -- The mock with a identifier version
   mock_Identifier_1 :: Maybe String -> Identifier
