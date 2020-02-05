a28cffe8c9a652e608e9233560d557e560d359f9
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2020-02-05 20:06:55 +0900

    Fix: 名前の間違い

--- hexyll-core/src/Hexyll/Core/Dependencies.hs
+++ hexyll-core/src/Hexyll/Core/Dependencies.hs
@@ -19,7 +19,7 @@ import           Data.Maybe ( fromMaybe )
 import qualified Data.Set   as S
 import           Data.Set   ( Set )
 
-import Control.Monad.Trans.RWS.Lazy ( RWS, rws, runRws )
+import Control.Monad.Trans.RWS.Lazy ( RWS, rws, runRWS )
 
 import Hexyll.Core.Identifier
 import Hexyll.Core.Identifier.Pattern
