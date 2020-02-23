7fb91c3ab15b45b93169baf2246f27d89f156680
Hexirp <Hexirp@users.noreply.github.com>
2020-02-23 18:16:16 +0900

    Add: H.C.Log を
    H.C.Logger の代替にして rio を基礎としたパターンの実装を目的とする。

---
+++ hexyll-core/src/Hexyll/Core/Log.hs
@@ -0,0 +1,62 @@
+module Hexyll.Core.Log where
+
+  import Prelude
+
+  import Data.Typeable
+
+  data LogLevel = LevelDebug | LevelInfo | LevelWarn | LevelError | LevelFatal
+    deriving ( Eq, Ord, Enum, Bounded, Show, Typeable )
+
+  type LogMessage = String
+
+  newtype LogEnv = LogEnv
+    { logFunc :: LogLevel -> LogMessage -> IO ()
+    } deriving ( Typeable )
+
+  sqLogEnv :: (LogLevel -> LogMessage -> IO ()) -> LogEnv
+  sqLogEnv f = f `seq` LogEnv f
+
+  class HasLogEnv env where
+    logEnvL :: Lens' env LogEnv
+
+  instance HasLogEnv LogEnv where
+    logEnvL = id
+
+  logGeneric
+    :: (MonadIO m, MonadReader env m, HasLogFunc env)
+    => LogLevel
+    -> LogMessage
+    -> m ()
+  logGeneric ll lm = do
+    le <- view logEnvL
+    liftIO $ logFunc le ll lm
+
+  logDebug
+    :: (MonadIO m, MonadReader env m, HasLogFunc env)
+    => LogMessage
+    -> m ()
+  logDebug lm = logGeneric LevelDebug lm
+
+  logInfo
+    :: (MonadIO m, MonadReader env m, HasLogFunc env)
+    => LogMessage
+    -> m ()
+  logInfo lm = logGeneric LevelInfo lm
+
+  logWarn
+    :: (MonadIO m, MonadReader env m, HasLogFunc env)
+    => LogMessage
+    -> m ()
+  logWarn lm = logGeneric LevelWarn lm
+
+  logError
+    :: (MonadIO m, MonadReader env m, HasLogFunc env)
+    => LogMessage
+    -> m ()
+  logError lm = logGeneric LevelError lm
+
+  logFatal
+    :: (MonadIO m, MonadReader env m, HasLogFunc env)
+    => LogMessage
+    -> m ()
+  logFatal lm = logGeneric LevelFatal lm
