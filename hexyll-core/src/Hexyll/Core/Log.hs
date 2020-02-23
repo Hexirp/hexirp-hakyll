022da1525e131da1e26eed6a269bb47c225c7a46
Hexirp <Hexirp@users.noreply.github.com>
2020-02-23 21:06:08 +0900

    Update: ドキュメントを付ける（完了）

--- hexyll-core/src/Hexyll/Core/Log.hs
+++ hexyll-core/src/Hexyll/Core/Log.hs
@@ -45,12 +45,19 @@ module Hexyll.Core.Log where
   sqLogEnv :: (LogLevel -> LogMessage -> IO ()) -> LogEnv
   sqLogEnv f = f `seq` LogEnv f
 
+  -- | Environment values with a logging function.
+  --
+  -- @since 0.1.0.0
   class HasLogEnv env where
     logEnvL :: Lens' env LogEnv
 
+  -- | @since 0.1.0.0
   instance HasLogEnv LogEnv where
     logEnvL = id
 
+  -- | Log a message with a given level.
+  --
+  -- @since 0.1.0.0
   logGeneric
     :: (MonadIO m, MonadReader env m, HasLogEnv env)
     => LogLevel
@@ -60,30 +67,45 @@ module Hexyll.Core.Log where
     env <- ask
     liftIO $ logFunc (view logEnvL env) ll lm
 
+  -- | Log a DEBUG level message.
+  --
+  -- @since 0.1.0.0
   logDebug
     :: (MonadIO m, MonadReader env m, HasLogEnv env)
     => LogMessage
     -> m ()
   logDebug lm = logGeneric LevelDebug lm
 
+  -- | Log a INFO level message.
+  --
+  -- @since 0.1.0.0
   logInfo
     :: (MonadIO m, MonadReader env m, HasLogEnv env)
     => LogMessage
     -> m ()
   logInfo lm = logGeneric LevelInfo lm
 
+  -- | Log a WARN level message.
+  --
+  -- @since 0.1.0.0
   logWarn
     :: (MonadIO m, MonadReader env m, HasLogEnv env)
     => LogMessage
     -> m ()
   logWarn lm = logGeneric LevelWarn lm
 
+  -- | Log a ERROR level message.
+  --
+  -- @since 0.1.0.0
   logError
     :: (MonadIO m, MonadReader env m, HasLogEnv env)
     => LogMessage
     -> m ()
   logError lm = logGeneric LevelError lm
 
+  -- | Log a FATAL level message.
+  --
+  -- @since 0.1.0.0
   logFatal
     :: (MonadIO m, MonadReader env m, HasLogEnv env)
     => LogMessage
