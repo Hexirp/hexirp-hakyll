34e3a306cef99094d924ab2aed5fe0e657b4b98c
Hexirp <Hexirp@users.noreply.github.com>
2020-02-29 12:54:39 +0900

    Fix: 型の情報を加える

--- hexyll-core/src/Hexyll/Core/Store.hs
+++ hexyll-core/src/Hexyll/Core/Store.hs
@@ -26,7 +26,10 @@ module Hexyll.Core.Store where
     save :: (Binary a, Typeable a) => StoreKey -> a -> m ()
     loadDelay :: (Binary a, Typeable a) => StoreKey -> m (StoreResult m a)
 
-  load :: MonadStore m => StoreKey -> m (Maybe (Either StoreError a))
+  load
+    :: (Binary a, Typeable a, MonadStore m)
+    => StoreKey
+    -> m (Maybe (Either StoreError a))
   load sk = do
     StoreResult mmesv <- loadDelay sk
     case mmesv of
@@ -36,4 +39,5 @@ module Hexyll.Core.Store where
   isExistent :: MonadStore m => StoreKey -> m Bool
   isExistent sk = do
     StoreResult mmesv <- loadDelay sk
+    let _type_info = StoreResult mmesv :: StoreResult m ()
     return $ isJust mmesv
