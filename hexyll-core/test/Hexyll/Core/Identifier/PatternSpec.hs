bb92e404278ef506a0b19519c7ef76c38df7b324
Hexirp <Hexirp@users.noreply.github.com>
2020-03-07 09:29:09 +0900

    Update: 進める

--- hexyll-core/test/Hexyll/Core/Identifier/PatternSpec.hs
+++ hexyll-core/test/Hexyll/Core/Identifier/PatternSpec.hs
@@ -24,3 +24,18 @@ module Hexyll.Core.Identifier.PatternSpec (spec) where
 
         it "nornally works ('index.md' with 'index.md')" $ do
           matchExpr "index.md" (fromGlob "index.md") `shouldBe` True
+
+        it "normally works ('index.md' with 'ind*.md')" $ do
+          matchExpr "index.md" (fromGlob "ind*.md") `shouldBe` True
+
+        it "normally works ('index.md' with 'assert.md')" $ do
+          matchExpr "index.md" (fromGlob "assert.md") `shouldBe` False
+
+        it "normally works ('index.md' with '*')" $ do
+          matchExpr "index.md" (fromGlob "*") `shouldBe` False
+
+        it "normally works ('foo/index.md' with '*/*.md')" $ do
+          matchExpr "foo/index.md" (fromGlob "*/*.md") `shouldBe` True
+
+        it "normally works ('foo/bar/alpha.md' with '**/alpha.md')" $ do
+          matchExpr "foo/bar/alpha.md" (fromGlob "**/alpha.md") `shouldBe` True
