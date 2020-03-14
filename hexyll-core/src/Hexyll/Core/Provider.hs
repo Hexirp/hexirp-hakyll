7312353c3e572b8af62c807224585fd856a02e88
Hexirp <Hexirp@users.noreply.github.com>
2020-03-14 16:10:36 +0900

    Add: getBody

--- hexyll-core/src/Hexyll/Core/Provider.hs
+++ hexyll-core/src/Hexyll/Core/Provider.hs
@@ -4,6 +4,8 @@ module Hexyll.Core.Provider where
 
   import Data.Time ( UTCTime (..) )
 
+  import qualified Data.ByteString.Lazy as BL
+
   import Hexyll.Core.Store
 
   import Path
@@ -13,6 +15,9 @@ module Hexyll.Core.Provider where
     , modificationTimeOld :: Maybe UTCTime
     }
 
+  newtype Body = Body { unBody :: BL.ByteString }
+
   class MonadStore m => MonadProvider m where
     getAllPath :: m [Path Rel File]
     getModificationTime :: Path Rel File -> m (Maybe ModificationTime)
+    getBody :: Path Rel File -> m (Maybe Body)
