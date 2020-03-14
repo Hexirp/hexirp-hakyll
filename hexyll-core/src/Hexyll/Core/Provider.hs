0476bb0b9b151af61848457a0c76b586f4122114
Hexirp <Hexirp@users.noreply.github.com>
2020-03-14 16:07:50 +0900

    Update: Identifier は不要である
    https://github.com/Hexirp/hexirp-hakyll/issues/111 の考察による。

--- hexyll-core/src/Hexyll/Core/Provider.hs
+++ hexyll-core/src/Hexyll/Core/Provider.hs
@@ -2,9 +2,17 @@ module Hexyll.Core.Provider where
 
   import Prelude
 
-  import Hexyll.Core.Identifier
-  import Hexyll.Core.Metadata
+  import Data.Time ( UTCTime (..) )
+
   import Hexyll.Core.Store
 
+  import Path
+
+  data ModificationTime = ModificationTime
+    { modificationTime    :: Maybe UTCTime
+    , modificationTimeOld :: Maybe UTCTime
+    }
+
   class MonadStore m => MonadProvider m where
-    getBody :: Identifier -> m String
+    getAllPath :: m [Path Rel File]
+    getModificationTime :: Path Rel File -> m (Maybe ModificationTime)
