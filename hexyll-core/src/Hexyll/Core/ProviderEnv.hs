3e5d11c680e91ab307cee5d0342210f745ebe555
Hexirp <Hexirp@users.noreply.github.com>
2020-03-16 10:58:01 +0900

    Update: ProviderEnv を完成させる

--- hexyll-core/src/Hexyll/Core/ProviderEnv.hs
+++ hexyll-core/src/Hexyll/Core/ProviderEnv.hs
@@ -2,6 +2,16 @@ module Hexyll.Core.ProviderEnv where
 
   import Prelude
 
+  import qualified Data.Set as S
+
+  import Path
+
   import Hexyll.Core.Provider
 
   data ProviderEnv = ProviderEnv
+    { providerGetAllPath :: IO (S.Set (Path Rel File))
+    , providerGetModificationTimeDelay
+        :: Path Rel File -> IO (Maybe (ProviderLoad IO ModificationTime))
+    , providerGetBodyDelay
+        :: Path Rel File -> IO (Maybe (ProviderLoad IO Body))
+    }
