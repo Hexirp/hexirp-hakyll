fadc7a0555a605889d12882b6367f7a07f084741
Hexirp <Hexirp@users.noreply.github.com>
2020-03-18 19:58:30 +0900

    Add: listDirectoryRecursive のテストを
    ゴリゴリ筋力な書き方をした。後でスマートに直す。

--- hexyll-core/test/System/Directory/HexyllSpec.hs
+++ hexyll-core/test/System/Directory/HexyllSpec.hs
@@ -5,7 +5,14 @@ module System.Directory.HexyllSpec (spec) where
   import Prelude
   import Test.Hspec
 
+  import Path
+
   import System.Directory.Hexyll
 
   spec :: Spec
-  spec = return ()
+  spec = do
+
+    describe "listDirectoryRecursive" $ do
+
+      it "works normally" $ do
+        (parseRelDir "./test-data/System/Directory/Hexyll/listDirectoryRecursive/" >>= listDirectoryRecursive >>= return . map toFilePath) `shouldReturn` ["test-data\\System\\Directory\\Hexyll\\listDirectoryRecursive\\beta.txt","test-data\\System\\Directory\\Hexyll\\listDirectoryRecursive\\alpha\\alpha.txt"]
