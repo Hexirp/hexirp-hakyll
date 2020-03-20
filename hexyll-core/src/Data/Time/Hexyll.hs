a91127669240800f75d083c03433da16a0c50d5d
Hexirp <Hexirp@users.noreply.github.com>
2020-03-20 12:45:08 +0900

    Update: インスタンスを追加

--- hexyll-core/src/Data/Time/Hexyll.hs
+++ hexyll-core/src/Data/Time/Hexyll.hs
@@ -2,11 +2,19 @@ module Data.Time.Hexyll where
 
   import Prelude
 
+  import Data.Typeable ( Typeable )
+
+  import Control.DeepSeq ( NFData (..) )
+
   import Data.Time
 
   import Data.Binary ( Binary (..) )
 
   newtype BinaryTime = BinaryTime { unBinaryTime :: UTCTime }
+    deriving ( Eq, Ord, Show, Typeable )
+
+  instance NFData BinaryTime where
+    rnf (BinaryTime x) = rnf x
 
   instance Binary BinaryTime where
     put (BinaryTime t) = case t of
