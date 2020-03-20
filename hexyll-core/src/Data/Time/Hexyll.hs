863409ccf395fb3d3c68a8183ed82d0bf5ec1ef3
Hexirp <Hexirp@users.noreply.github.com>
2020-03-20 12:54:16 +0900

    Update: Data.Time.Hexyll の doc を

--- hexyll-core/src/Data/Time/Hexyll.hs
+++ hexyll-core/src/Data/Time/Hexyll.hs
@@ -1,3 +1,14 @@
+-- |
+-- Module:      Data.Time.Hexyll
+-- Copyright:   (c) 2019 Hexirp
+-- License:     Apache-2.0
+-- Maintainer:  https://github.com/Hexirp/hexirp-hakyll
+-- Stability:   unstable
+-- Portability: portable
+--
+-- This module includes types and functions about "Data.Time".
+--
+-- @since 0.1.0.0
 module Data.Time.Hexyll where
 
   import Prelude
@@ -10,12 +21,17 @@ module Data.Time.Hexyll where
 
   import Data.Binary ( Binary (..) )
 
+  -- | A wrapper for the instance @'Binary' 'UTCTime'@.
+  --
+  -- @since 0.1.0.0
   newtype BinaryTime = BinaryTime { unBinaryTime :: UTCTime }
     deriving ( Eq, Ord, Show, Typeable )
 
+  -- | @since 0.1.0.0
   instance NFData BinaryTime where
     rnf (BinaryTime x) = rnf x
 
+  -- | @since 0.1.0.0
   instance Binary BinaryTime where
     put (BinaryTime t) = case t of
       UTCTime (ModifiedJulianDay d) dt -> do
