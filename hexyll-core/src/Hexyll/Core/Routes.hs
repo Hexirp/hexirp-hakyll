8b14ceced4aad7eee0ae7429d15f643a6a51b4ff
Hexirp <Hexirp@users.noreply.github.com>
2020-03-20 16:53:08 +0900

    Update: インスタンスを追加する

--- hexyll-core/src/Hexyll/Core/Routes.hs
+++ hexyll-core/src/Hexyll/Core/Routes.hs
@@ -2,11 +2,19 @@ module Hexyll.Core.Routes where
 
   import Prelude
 
+  import Data.Typeable ( Typeable )
+
+  import Control.DeepSeq ( NFData (..) )
+
   import Path
 
   import Hexyll.Core.Identifier
 
   newtype Routes = Routes { unRoutes :: Identifier -> [Path Rel File] }
+    deriving ( Typeable )
+
+  instance NFData Routes where
+    rnf (Routes x) = rnf x
 
   instance Semigroup Routes where
     Routes f <> Routes g = Routes (\i -> f i ++ g i)
