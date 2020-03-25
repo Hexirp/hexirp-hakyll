8c50a1e7cc2ead8297c623a5e2abc244700a5947
Hexirp <Hexirp@users.noreply.github.com>
2020-03-25 16:30:18 +0900

    Update: とりあえずコンパイルを通す

--- hexyll-core/src/Hexyll/Core/Compiler.hs
+++ hexyll-core/src/Hexyll/Core/Compiler.hs
@@ -29,13 +29,22 @@ module Hexyll.Core.Compiler where
       f1  x1             y1            = f2 <$> x1 <*> y1
       f2 :: forall s m a b. (Functor s, Applicative m) => Either (s (Coroutine s m (a -> b))) (a -> b) -> Either (s (Coroutine s m a)) a -> Either (s (Coroutine s m b)) b
       f2 (Left x2     )  y2            = Left (f3 x2 y2)
-      f2 (Right x2    )  y2            = undefined
+      f2 (Right x2    ) (Left y2     ) = Left (f7 x2 y2)
+      f2 (Right x2    ) (Right y2    ) = Right (f9 x2 y2)
       f3 :: forall s m a b. (Functor s, Applicative m) => s (Coroutine s m (a -> b)) -> Either (s (Coroutine s m a)) a -> s (Coroutine s m b)
       f3  x3             y3            = f4 x3 (pure y3)
       f4 :: forall s m a b. (Functor s, Applicative m) => s (Coroutine s m (a -> b)) -> m (Either (s (Coroutine s m a)) a) -> s (Coroutine s m b)
       f4  x4             y4            = f5 x4 (Coroutine y4)
       f5 :: forall s m a b. (Functor s, Applicative m) => s (Coroutine s m (a -> b)) -> Coroutine s m a -> s (Coroutine s m b)
-      f5  x5             y5            = fmap (<*> y5) x5
+      f5  x5             y5            = fmap (\x' -> f6 x' y5) x5
+      f6 :: forall s m a b. (Functor s, Applicative m) => Coroutine s m (a -> b) -> Coroutine s m a -> Coroutine s m b
+      f6  x6             y6            = f0 x6 y6
+      f7 :: forall s m a b. (Functor s, Applicative m) => (a -> b) -> s (Coroutine s m a) -> s (Coroutine s m b)
+      f7  x7             y7            = fmap (f8 x7) y7
+      f8 :: forall s m a b. (Functor s, Applicative m) => (a -> b) -> Coroutine s m a -> Coroutine s m b
+      f8  x8             y8            = fmap x8 y8
+      f9 :: forall a b. (a -> b) -> a -> b
+      f9  x9             y9            = x9 y9
 
   type Snapshot = String
 
