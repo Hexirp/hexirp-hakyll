f9da5ea2764b81c0fc5659c566d74405edd9f7c1
Hexirp <Hexirp@users.noreply.github.com>
2020-03-25 14:12:57 +0900

    Update: 途中

--- hexyll-core/src/Hexyll/Core/Compiler.hs
+++ hexyll-core/src/Hexyll/Core/Compiler.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ExplicitForAll #-}
+
 module Hexyll.Core.Compiler where
 
   import Prelude
@@ -17,6 +19,23 @@ module Hexyll.Core.Compiler where
       f2 (Left s     ) = Left (f3 s)
       f2 (Right x    ) = Right (f x)
       f3  x            = fmap f0 x
+
+  instance (Functor s, Applicative m) => Applicative (Coroutine s m) where
+    pure x = Coroutine (pure (Right x))
+    x <*> y = f0 x y where
+      f0 :: forall s m a b. (Functor s, Applicative m) => Coroutine s m (a -> b) -> Coroutine s m a -> Coroutine s m b
+      f0 (Coroutine x0) (Coroutine y0) = Coroutine (f1 x0 y0)
+      f1 :: forall s m a b. (Functor s, Applicative m) => m (Either (s (Coroutine s m (a -> b))) (a -> b)) -> m (Either (s (Coroutine s m a)) a) -> m (Either (s (Coroutine s m b)) b)
+      f1  x1             y1            = f2 <$> x1 <*> y1
+      f2 :: forall s m a b. (Functor s, Applicative m) => Either (s (Coroutine s m (a -> b))) (a -> b) -> Either (s (Coroutine s m a)) a -> Either (s (Coroutine s m b)) b
+      f2 (Left x2     )  y2            = Left (f3 x2 y2)
+      f2 (Right x2    )  y2            = undefined
+      f3 :: forall s m a b. (Functor s, Applicative m) => s (Coroutine s m (a -> b)) -> Either (s (Coroutine s m a)) a -> s (Coroutine s m b)
+      f3  x3             y3            = f4 x3 (pure y3)
+      f4 :: forall s m a b. (Functor s, Applicative m) => s (Coroutine s m (a -> b)) -> m (Either (s (Coroutine s m a)) a) -> s (Coroutine s m b)
+      f4  x4             y4            = f5 x4 (Coroutine y4)
+      f5 :: forall s m a b. (Functor s, Applicative m) => s (Coroutine s m (a -> b)) -> Coroutine s m a -> s (Coroutine s m b)
+      f5  x5             y5            = fmap (<*> y5) x5
 
   type Snapshot = String
 
