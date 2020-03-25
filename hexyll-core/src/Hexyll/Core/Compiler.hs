ab27caff09f6aadd5ae2d97eca3d2432a2dd9bf5
Hexirp <Hexirp@users.noreply.github.com>
2020-03-25 13:16:19 +0900

    Add: Functor (Coroutine s m)

--- hexyll-core/src/Hexyll/Core/Compiler.hs
+++ hexyll-core/src/Hexyll/Core/Compiler.hs
@@ -10,6 +10,14 @@ module Hexyll.Core.Compiler where
     { unCoroutine :: m (Either (s (Coroutine s m a)) a)
     }
 
+  instance (Functor s, Functor m) => Functor (Coroutine s m) where
+    fmap f = f0 where
+      f0 (Coroutine x) = Coroutine (f1 x)
+      f1  x            = fmap f2 x
+      f2 (Left s     ) = Left (f3 s)
+      f2 (Right x    ) = Right (f x)
+      f3  x            = fmap f0 x
+
   type Snapshot = String
 
   data CompilerErrors a = CompilerErrors
