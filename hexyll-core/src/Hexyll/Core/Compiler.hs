9b01d6784cc18165b29b5d354d0ad742c55987e7
Hexirp <Hexirp@users.noreply.github.com>
2020-03-25 17:41:14 +0900

    Update: 整理

--- hexyll-core/src/Hexyll/Core/Compiler.hs
+++ hexyll-core/src/Hexyll/Core/Compiler.hs
@@ -24,19 +24,18 @@ module Hexyll.Core.Compiler where
       -- See https://github.com/ekmett/free/blob/7be30d7cd139a7f40a5e76cd3cb126534c239231/src/Control/Monad/Free.hs#L219-L225 .
       -- That is the implementation of 'Functor f => Applicative (Free f)' in free-5.1.3. This implementation is based on it.
       f0 :: forall s m a b. (Functor s, Applicative m) => Coroutine s m (a -> b) -> Coroutine s m a -> Coroutine s m b
-      f0 (Coroutine x0) (Coroutine y0) = Coroutine (f2 <$> x0 <*> y0)
+      f0 x0 y0 = Coroutine (f2 <$> unCoroutine x0 <*> unCoroutine y0)
       f2 :: forall s m a b. (Functor s, Applicative m) => Either (s (Coroutine s m (a -> b))) (a -> b) -> Either (s (Coroutine s m a)) a -> Either (s (Coroutine s m b)) b
-      f2 (Left x2     )  y2            = Left (f3 x2 (Coroutine (pure y2)))
-      f2 (Right x2    ) (Left y2     ) = Left (fmap (fmap x2) y2)
-      f2 (Right x2    ) (Right y2    ) = Right (x2 y2)
-      f3 :: forall s m a b. (Functor s, Applicative m) => s (Coroutine s m (a -> b)) -> Coroutine s m a -> s (Coroutine s m b)
-      f3  x3             y3            = fmap (\x' -> f0 x' y3) x3
+      f2 (Left x2 )  y2        = Left (fmap (flip f0 (Coroutine (pure y2))) x2)
+      f2 (Right x2) (Left y2 ) = Left (fmap (fmap x2) y2)
+      f2 (Right x2) (Right y2) = Right (x2 y2)
 
   instance (Functor s, Monad m) => Monad (Coroutine s m) where
     x >>= y = f0 x y where
-      f0 (Coroutine x0) y0 = Coroutine (f1 x0 y0)
-      f1 x1 y1 = x1 >>= \x' -> f2 x' y1
-      f2 (Left x2) y2 = pure (Left (fmap (\x' -> f0 x' y2) x2))
+      f0 :: forall s m a b. (Functor s, Monad m) => Coroutine s m a -> (a -> Coroutine s m b) -> Coroutine s m b
+      f0 x0 y0 = Coroutine (unCoroutine x0 >>= flip f2 y0)
+      f2 :: forall s m a b. (Functor s, Monad m) => Either (s (Coroutine s m a)) a -> (a -> Coroutine s m b) -> m (Either (s (Coroutine s m b)) b)
+      f2 (Left x2)  y2 = pure (Left (fmap (flip f0 y2) x2))
       f2 (Right x2) y2 = unCoroutine (y2 x2)
 
   type Snapshot = String
