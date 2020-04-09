33a7b76adaa9f99f9a0248b71a78b28b7fba219e
Hexirp <Hexirp@users.noreply.github.com>
2020-04-09 14:20:29 +0900

    Add: ドキュメントの一部を

--- hexyll-core/src/Hexyll/Core/Item.hs
+++ hexyll-core/src/Hexyll/Core/Item.hs
@@ -9,20 +9,28 @@ module Hexyll.Core.Item where
   import Hexyll.Core.Identifier
 
   -- | An item is a combination of some content and its 'Identifier'.
+  --
+  -- It's other type represents compilation results.
+  --
+  -- @since 0.1.0.0
   data Item a = Item
     { itemIdentifier :: !Identifier
     , itemBody       :: !a
     } deriving (Eq, Ord, Show, Typeable)
 
+  -- | @since 0.1.0.0
   instance Functor Item where
     fmap f (Item i x) = Item i (f x)
 
+  -- | @since 0.1.0.0
   instance Foldable Item where
     foldr f z (Item _ x) = f x z
 
+  -- | @since 0.1.0.0
   instance Traversable Item where
     traverse f (Item i x) = Item i <$> f x
 
+  -- | @since 0.1.0.0
   instance Binary a => Binary (Item a) where
     put (Item i x) = do
       put i
