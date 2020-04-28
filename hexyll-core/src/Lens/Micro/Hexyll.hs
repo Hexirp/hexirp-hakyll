47c97261ff6e574f08f4395d521b559ac147bd75
Hexirp <Hexirp@users.noreply.github.com>
2020-04-28 14:58:51 +0900

    Add: Lens.Micro.Hexyll を
    Fix https://github.com/Hexirp/hexirp-hakyll/issues/144 .
    
    ドキュメントに書いたように microlens-mtl に全く同じ内容の関数があるが、
    名前の関係で独自に定義しておく。

---
+++ hexyll-core/src/Lens/Micro/Hexyll.hs
@@ -0,0 +1,31 @@
+-- |
+-- Module:      Lens.Micro.Hexyll
+-- Copyright:   (c) 2020 Hexirp
+-- License:     Apache-2.0
+-- Maintainer:  https://github.com/Hexirp/hexirp-hakyll
+-- Stability:   unstable
+-- Portability: portable
+--
+-- This module includes additional functions of "Lens.Micro" and
+-- "Lens.Micro.Hexyll".
+--
+-- @since 0.1.0.0
+module Lens.Micro.Hexyll where
+
+  import Prelude
+
+  import Control.Monad.Reader.Class (MonadReader (ask))
+
+  import Lens.Micro       (Getting)
+  import Lens.Micro.Extra (view)
+
+  -- | Ask the environment and apply a getter to it.
+  --
+  -- I know there is a function @view@ in @Lens.Micro.Mtl@ in <http://hackage.haskell.org/package/microlens-mtl microlens-mtl>.
+  -- But I want a better named function.
+  --
+  -- @since 0.1.0.0
+  askView :: MonadReader env m => Getting a env a -> m a
+  askView l = do
+    env <- ask
+    return $ view l env
